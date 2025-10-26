7c2603e3b573241d07c82d604e0f9252d0ea4d25
Emilly Ranny <ranny.emilly@discente.ufg.br>
2025-10-26 17:04:10 -0300

    Create Matrizes_Resumo.c

---
+++ AeD1/Aula/Matrizes_Resumo.c
@@ -0,0 +1,31 @@
+//Matrizes - Arrays bidimensionais
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int linha, i, j, coluna;
+    scanf("%d %d", &linha, &coluna);
+    
+    int vet[linha][coluna];
+    
+    for(i = 0; i < linha; i++){
+        for(j = 0; j < coluna; j++){
+        printf("Insira o valor da linha no indice %d, e coluna no indice  %d\n", i, j);
+            scanf("%d ", &vet[i][j]);
+        }
+          
+    }
+    
+        for(i = 0; i < linha; i++){
+            for(j = 0; j < coluna; j++){
+                printf("%d ", vet[i][j]);
+              
+            }
+              printf("\n");
+        }
+        
+    
+    
+    return 0;
+}
